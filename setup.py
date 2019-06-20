5e39a3d07dc1c1fa126e2a2f4fc8a2b3a0911b31
mlee3142 <mil@mit.edu>
2019-06-20 18:14:36 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='PySS',
+      version='0.01',
+      description='State-space model fits written in Python',
+      url='https://github.com/mlee3142/PySS',
+      author='mil',
+      author_email='none@none.com',
+      license='MIT',
+      packages=['PySS'],
+      install_requires=[],
+      zip_safe=False)
